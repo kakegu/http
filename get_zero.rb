eb6a769c8a8e7f9c9c98ce30e9915415af6108e7
makeabignews <makeabignews@sina.com>
2018-11-28 22:08:42 -0500

    up

---
+++ get_zero.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+require 'json'
+
+def send_data(url,hash_data)  
+    uri = URI(url)
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = hash_data.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        http.request(req).body
+    end
+end
+
+address = ARGV[0]
+amount = ARGV[1].to_i*1000000
+
+hash_data = { 
+    :jsonrpc => '2.0', 
+    :id => 1,
+    :method => 'sendtoaddress' ,
+    :params => [address,amount]
+  }
+
+
+puts send_data("http://localhost:6552",hash_data);
